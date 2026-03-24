d29b823832159f0d53271f416b0c3b6d6fcba2b2
nikolas <nikosnick13@live.com>
2026-03-24 14:37:20 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/cf/eduapp/Edu9RestAppProApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edu9RestAppProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edu9RestAppProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/eduapp/Edu9RestAppProApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eduapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edu9RestAppProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
