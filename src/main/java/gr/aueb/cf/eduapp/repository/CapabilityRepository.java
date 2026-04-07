1b899dd5187ef64c209432a494985812117fa1c2
nikolas <nikosnick13@live.com>
2026-04-07 09:31:48 +0300

    fix: add repositories

---
+++ src/main/java/gr/aueb/cf/eduapp/repository/CapabilityRepository.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.Capability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapabilityRepository extends JpaRepository<Capability,Long> {
+
+}
---
+++ src/main/java/gr/aueb/cf/eduapp/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository  extends JpaRepository<Role,Long> {
+
+}
