c3d11a9a3bb87733b7298b1b1c1463a5e024cee1
nikolas <nikosnick13@live.com>
2026-04-14 11:06:41 +0300

    chore: new comment

--- src/main/java/gr/aueb/cf/eduapp/service/UserService.java
+++ src/main/java/gr/aueb/cf/eduapp/service/UserService.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor // Είναι για το DI
 @Slf4j
 public class UserService implements IUserService{
 
